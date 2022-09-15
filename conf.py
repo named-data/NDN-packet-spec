d4f94a3e9642de43ccb49907f96bbd24542cd421
Davide Pesavento <davidepesa@gmail.com>
2022-09-15 18:06:07 -0400

    Update furo; simplify conf.py
    Change-Id: I8f098441051dc92378dfc9ce57287c0b8e2bd621

--- conf.py
+++ conf.py
@@ -1,21 +1,10 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'NDN Packet Format Specification'
 copyright = '2013-2022, Named Data Networking Project'
@@ -28,33 +17,18 @@ release = '0.3'
 
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# If your documentation needs a minimal Sphinx version, state it here.
-#
 needs_sphinx = '4.0'
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = []
 
-# The master toctree document.
-master_doc = 'index'
-
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = 'furo'
 
 # Add any paths that contain custom static files (such as style sheets) here,
@@ -68,13 +42,13 @@ html_copy_source = False
 html_show_sourcelink = False
 
 html_theme_options = {
-    "light_css_variables": {
-        "color-brand-primary": "#fd7800",
-        "color-brand-content": "#fd7800",
+    'light_css_variables': {
+        'color-brand-primary': '#fd7800',
+        'color-brand-content': '#fd7800',
     },
-    "dark_css_variables": {
-        "color-brand-primary": "#fd7800",
-        "color-brand-content": "#fd7800",
+    'dark_css_variables': {
+        'color-brand-primary': '#fd7800',
+        'color-brand-content': '#fd7800',
     },
 }
 
