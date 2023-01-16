64fa06747df07b9636bf55c035e2edd9307c9894
Davide Pesavento <davidepesa@gmail.com>
2023-01-15 23:02:24 -0500

    Add workflow for weekly linkcheck
    Change-Id: I6b45566688b7d975347fa6a278d70c007fa35780

--- conf.py
+++ conf.py
@@ -3,6 +3,8 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import sphinx
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -24,6 +26,9 @@ extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# User-Agent header sent with linkcheck HTTP requests.
+user_agent = f'Sphinx/{sphinx.__version__}'
 
 # Use ABNF syntax highlighting by default for code blocks.
 highlight_language = 'abnf'
