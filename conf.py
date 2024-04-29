663e9b667ab2e03bcf6aeea28677ae6321916827
Davide Pesavento <davidepesa@gmail.com>
2024-04-28 22:40:03 -0400

    Adjust CSS colors
    The new colors are designed to stay reasonably close to the
    original #fd7800 and the #bf4818 used in the SVG logo, while
    greatly improving the contrast ratio to ensure a minimum of
    4.5:1 for the light theme and 7:1 for the dark theme.

--- conf.py
+++ conf.py
@@ -52,12 +52,14 @@ html_show_sourcelink = False
 
 html_theme_options = {
     'light_css_variables': {
-        'color-brand-primary': '#fd7800',
-        'color-brand-content': '#fd7800',
+        'color-brand-primary': '#d43b0b',
+        'color-brand-content': '#c75300',
+        'color-brand-visited': '#a55922',
     },
     'dark_css_variables': {
-        'color-brand-primary': '#fd7800',
-        'color-brand-content': '#fd7800',
+        'color-brand-primary': '#fd861a',
+        'color-brand-content': '#f87e00',
+        'color-brand-visited': '#dd8d48',
     },
     'source_repository': 'https://github.com/named-data/NDN-packet-spec',
     'source_branch': 'master',
